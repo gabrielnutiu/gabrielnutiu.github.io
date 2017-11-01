661829e91c4691ba485cf4109089c0723f114cd9
gabrielnutiu <gabrielnutiu@yahoo.com>
2017-11-01 08:46:12 -0700

    canvas close path

--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
 
-document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.0";
+document.getElementById("id_logic_level_version").innerHTML = "Logic level version: 2017.11.01.1";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -9,4 +9,5 @@ context.beginPath();
 context.moveTo(10, 10);
 context.lineTo(100, 200);
 context.lineTo(200, 100);
+context.closePath();
 context.stroke();

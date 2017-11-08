f3a450dde2ad3f61150daca388cc416e1e247305
gabrielnutiu <gabrielnutiu@yahoo.com>
2017-11-08 08:17:52 -0800

    generare culoare aleatoare

--- touch.js
+++ touch.js
@@ -1,12 +1,22 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.2";
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.08.3";
 
 	var canvas = document.getElementById("id_canvas");
 	var context = canvas.getContext("2d");
 	
 	canvas.addEventListener("touchstart", on_touch_start);
 	
+	function generate_random_color()
+	{
+		var litere = "0123456789ABCDEF";
+		var color = "#";
+		for(i = 0; i < 6; i++)
+			color +=litere[Math.floor(Math.random() * 16)];
+		return color;
+	}
+//------------------------------------------------------------------------------------	
 	function on_touch_start(e)
 	{
+		var rect = canvas.getBoundingClientRect();
 		var touches = e.changedTouches;
 		for(i = 0; i < touches.length; i++)
 		{

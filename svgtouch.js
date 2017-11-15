22ecc0c3ee8d3e56139c579f71b6770840fb76dd
gabrielnutiu <gabrielnutiu@yahoo.com>
2017-11-15 07:53:46 -0800

    svg_touch

---
+++ svgtouch.js
@@ -0,0 +1,19 @@
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.0";
+
+addEventListener("touchstart", on_touch_start);
+
+function on_touch_start(e)
+{
+	var touches = e.changedTouches;
+	for(var i = 0; i < touches.length; i++)
+	{
+		var circle = document.crateElementNS("http://www.w3.org/2000/svg","circle");
+		circle.setAttribute("cx", touches[i].pageX);
+		circle.setAttribute("cy", touches[i].pageY);
+		circle.setAttribute("r", 10);
+		var svg = document.getElementById("id_svg");
+		svg.appendElement(circle);
+	}
+	
+}
+//--------------------------------------------------------------

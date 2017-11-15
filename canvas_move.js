e35becaf631ef61c03ff2cbaa444c680ce777a57
gabrielnutiu <gabrielnutiu@yahoo.com>
2017-11-15 09:42:44 -0800

    adding img onload

--- canvas_move.js
+++ canvas_move.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.1";
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.15.2";
 
 
 var canvas = document.getElementById("id_canvas");
@@ -6,9 +6,10 @@ var context = canvas.getContext("2d");
 var rect_canvas = canvas.getBoundingClientRect();
 var img = document.getElementById("id_img");
 
+img.onload = function(){
 context.drawImage(img, 100, 100, 100, 100);
 var rect_img = img.getBoundingClientRect();
-
+}
 canvas.addEventListener("touchmove", on_touch_move);
 
 //------------------------------------------------------------------

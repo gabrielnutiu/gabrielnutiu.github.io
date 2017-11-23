2a28a39e766ead55e79274121ce0a711d772b1d0
gabrielnutiu <gabrielnutiu@yahoo.com>
2017-11-22 18:50:15 -0800

    speech Syntheseis

---
+++ synt.js
@@ -0,0 +1,12 @@
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.0";
+
+
+var synt = window.speachSyntheseis;
+//---------------------------------------------------------------------------------------
+function get_voices()
+{
+	var voices = synt.getVoices();
+	for(var i = 0; i < voices.length; i++)
+		document.getElementById("id_voices").innerHTML = voices[i].name + ":" voives[i].lang + "<br>";
+}
+//---------------------------------------------------------------------------------------

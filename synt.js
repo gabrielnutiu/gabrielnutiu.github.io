e225c94fc7385b32bfaa20cdb1db8db1e9ea6757
gabrielnutiu <gabrielnutiu@yahoo.com>
2017-11-22 19:17:18 -0800

    another bug

--- synt.js
+++ synt.js
@@ -1,11 +1,11 @@
-document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.4";
+document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.5";
 
 
 var synt = window.speechSynthesis;
 //---------------------------------------------------------------------------------------
 function get_voices()
 {
-	var voices = synt.get_Voices();
+	var voices = synt.getVoices();
 	for(var i = 0; i < voices.length; i++)
 		document.getElementById("id_voices").innerHTML = voices[i].name + ":" + voices[i].lang + "<br>";
 }

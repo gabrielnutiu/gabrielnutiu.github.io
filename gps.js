ce2de52a4c5c48c8ef002c581ae2f07fc91e1b1b
gabrielnutiu <gabrielnutiu@yahoo.com>
2017-12-06 17:49:08 -0800

    adding altitude

--- gps.js
+++ gps.js
@@ -6,7 +6,11 @@ navigator.geolocation.getCurrentPosition(on_gps_success, on_gps_error);
 
 function on_gps_success(p)
 {
-		document.getElementById("id_p").innerHTML ="lat = "+ p.coords.latitude + "long = "+p.coords.longitude + "accuracy = " + p.coords.accuracy +"m";
+		document.getElementById("id_p").innerHTML =
+		"lat = "+ p.coords.latitude + 
+		"<br>long = "+p.coords.longitude +
+		"<br>accuracy = " + p.coords.accuracy +"m"+
+		"<br> altitude = "+ p.coords.altitude +"m";
 }
 
 //--------------------------------------------------

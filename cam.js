39b09b26cd3fafdca50eef990eb5a92524a34f6a
gabrielnutiu <gabrielnutiu@yahoo.com>
2018-01-10 18:23:17 -0800

    add function snap

--- cam.js
+++ cam.js
@@ -1,10 +1,11 @@
 var d = new Date();
-document.getElementById("id_logic_level_version").innerHTML = "Business level version:"+d.getFullYear() + "." +d.getMonth() + 1 +"." +d.getDate()+".1";
+document.getElementById("id_logic_level_version").innerHTML = "Business level version:"+d.getFullYear() + "." +d.getMonth() + 1 +"." +d.getDate()+".2";
 
 var constraints = {audio: false, video:{facingMode :"environment"}};
 navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error);
 
 var video = document.getElementById("id_video");
+video.addEventListener("touchstart", snap);
 //-----------------------------------------------
 function on_success(stream)
 {
@@ -13,8 +14,17 @@ function on_success(stream)
 }
 //-----------------------------------------------
 function on_error(error)
+{	
+	alert("Error");
+	
+}
+//------------------------------------------------
+function snap ()
 {
-		alert("Error");
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2D");
+	context.drawImage( video, 0, 0, 640, 480);
+		
 	
 }
 //------------------------------------------------

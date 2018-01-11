d493599e251e117c594e099fd9fa6418866cada5
gabrielnutiu <gabrielnutiu@yahoo.com>
2018-01-10 19:19:10 -0800

    vibrate pack

--- vibrate.js
+++ vibrate.js
@@ -1,10 +1,11 @@
 var d = new Date();
-document.getElementById("id_logic_level_version").innerHTML = "Business level version:"+d.getFullYear() + "." +d.getMonth() + 1 +"." +d.getDate()+".0";
+document.getElementById("id_logic_level_version").innerHTML = "Business level version:"+d.getFullYear() + "." +d.getMonth() + 1 +"." +d.getDate()+".1";
 
 document.addEventListener("touchstart", vibrate);
 //---------------------------
 function vibrate()
 {
-	window.navigator.vibrate(200);
+	window.navigator.vibrate([200, 200, 500, 200, 1000]);
 		
 }
+-

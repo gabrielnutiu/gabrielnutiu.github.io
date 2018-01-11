1a3b0f53c4cd566280462789cc3609f97c9fa9ea
gabrielnutiu <gabrielnutiu@yahoo.com>
2018-01-10 19:33:48 -0800

    start/stop vibrate

--- vibrate.js
+++ vibrate.js
@@ -1,10 +1,23 @@
 var d = new Date();
-document.getElementById("id_logic_level_version").innerHTML = "Business level version:"+d.getFullYear() + "." +d.getMonth() + 1 +"." +d.getDate()+".1";
+document.getElementById("id_logic_level_version").innerHTML = "Business level version:"+d.getFullYear() + "." +d.getMonth() + 1 +"." +d.getDate()+".2";
 
-document.addEventListener("touchstart", vibrate);
-//---------------------------
+document.addEventListener("touchstart", start_timer);
+document.addEventListener("touchend", stop_timer);
+var timer_id;
+//-----------------------------
+function start_timer()
+{
+	timer_id = setInterval(vibrate, 200);
+}
+
+//-----------------------------
+function stop_timer()
+{
+	clearInterval(timer_id);
+}
+//-----------------------------
 function vibrate()
 {
-	window.navigator.vibrate([200, 200, 500, 200, 1000]);
+	window.navigator.vibrate(300);
 		
 }

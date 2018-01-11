98759f66f96b8f9602324c20c366583b3a5d550b
gabrielnutiu <gabrielnutiu@yahoo.com>
2018-01-10 19:43:48 -0800

    remove business level

--- vibrate.js
+++ vibrate.js
@@ -1,8 +1,7 @@
-var d = new Date();
-document.getElementById("id_logic_level_version").innerHTML = "Business level version:"+d.getFullYear() + "." +d.getMonth() + 1 +"." +d.getDate()+".3";
 
 document.addEventListener("touchstart", start_timer);
 document.addEventListener("touchend", stop_timer);
+
 var timer_id;
 //-----------------------------
 function start_timer()
